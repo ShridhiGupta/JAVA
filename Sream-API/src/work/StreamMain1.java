36c8672c9d0cbcc047d3f53fcc9df9ba709b499f
ShridhiGupta <guptashridhi11@gmail.com>
2026-05-27 19:36:21 +0530

    Added some more code because explored some new concepts

---
+++ Sream-API/src/work/StreamMain1.java
@@ -0,0 +1,15 @@
+package work;
+
+import java.util.*;
+
+public class StreamMain1 {
+    public static void main(String[] args) {
+
+
+        // Create a list and filter all even numbers from the list
+
+
+        List<Integer> list1 = List.of(2, 4, 50, 21, 22, 67);
+
+    }
+}
