a6d9e554d9c82a718032c18135cb2215e39244c8
ShridhiGupta <guptashridhi11@gmail.com>
2026-08-18 08:26:20 +0530

    Added the genericClass, markerClass, nestedClass

--- Java_OOPs/exception/FoodDeliveryApp_tryCatch.java
+++ Java_OOPs/exception/FoodDeliveryApp_tryCatch.java
@@ -5,11 +5,8 @@ public class FoodDeliveryApp_tryCatch {
     public static void main(String[] args) {
 
         try {
-
             int amount = 0;
-
             int result = 500 / amount;
-
             System.out.println("Payment successful: ₹" + result);
 
         } catch (ArithmeticException e) {
